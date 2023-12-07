0fe1d9f331b37b14cda11c50cee31f81760e86a5
40259039 <rflynn12@qub.ac.uk>
2023-12-07 13:16:22 +0000

    Fix ip issue on lab machines
    Another fix attempt

--- Client/Extensions/NetworkExtensions.cs
+++ Client/Extensions/NetworkExtensions.cs
@@ -8,14 +8,37 @@ namespace P2PProject.Client.Extensions
     {        
         public static IEnumerable<IPAddress> GetLocalIPAddress()
         {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
+            var ips = new List<IPAddress>();
+            try
             {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (var ip in host.AddressList)
                 {
-                    yield return ip;
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        ips.Add(ip);
+                    }
                 }
-            }   
+            }
+            catch(Exception e) {  }
+            finally
+            {
+                if(ips.Count == 0)
+                {
+                    bool validIp = false;
+                    while (!validIp)
+                    {
+                        Console.WriteLine("Error getting local IP address.\nPlease enter your local IP address");
+                        var ipString = Console.ReadLine();
+                        if(IPAddress.TryParse(ipString, out var ip))
+                        {
+                            ips.Add(ip);
+                            validIp = true;
+                        }
+                    }
+                }
+            }
+            return ips;
         }
 
         public static NodeInfo ToNodeInfo(this IPEndPoint ep, Guid id, string nodeName) => new NodeInfo(ep.Address.ToString(), ep.Port, id, nodeName);
